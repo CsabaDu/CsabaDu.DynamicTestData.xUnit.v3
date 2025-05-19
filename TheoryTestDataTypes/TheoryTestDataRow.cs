37a526b6ef15ee495a39891b211cbb613967ad62
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-05-19 14:49:15 +0200

    minor namespace

--- TheoryTestDataTypes/TheoryTestDataRow.cs
+++ TheoryTestDataTypes/TheoryTestDataRow.cs
@@ -1,8 +1,6 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025. Csaba Dudas (CsabaDu)
 
-using System.ComponentModel;
-
 namespace CsabaDu.DynamicTestData.xUnit.v3.TheoryTestDataTypes;
 
 /// <summary>
